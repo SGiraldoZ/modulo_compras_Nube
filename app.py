40eb7f65b976037853a9fcfd3b8a5cb90234ed65
Sebas <sebasgizu@gmail.com>
2023-08-23 19:21:56 -0500

    Add gitignore modules to handle .env variables

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    print(os.environ['TUMAMA'])
