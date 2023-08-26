29d752092143a0b92ebca64d404d4b99e7c38ae1
Sebas <sebasgizu@gmail.com>
2023-08-25 20:29:10 -0500

    Change Database structure. Only Store purchases

--- DB_Create.sql
+++ DB_Create.sql
@@ -2,26 +2,10 @@ CREATE DATABASE UDemy;
 
 \c UDemy;
 
-CREATE TABLE Users (
-    u_id SERIAL NOT NULL UNIQUE,
-    token VARCHAR,
-    username VARCHAR,
-    CONSTRAINT user_pk PRIMARY KEY (u_id)
-);
-
-CREATE TABLE Courses (
-    course_id SERIAL NOT NULL UNIQUE,
-    cms_id VARCHAR NOT NULL UNIQUE,
-    course_name VARCHAR,
-    CONSTRAINT course_pk PRIMARY KEY (course_id)
-);
-
 CREATE TABLE Purchases (
-    u_id INT NOT NULL UNIQUE,
-    course_id INT NOT NULL UNIQUE,
+    user_token VARCHAR NOT NULL,
+    course_cms_id VARCHAR NOT NULL,
     price FLOAT,
     purchase_date DATE,
-    CONSTRAINT Purchase_pk PRIMARY KEY (u_id, course_id),
-    CONSTRAINT purchase_user_fk FOREIGN KEY (u_id) REFERENCES Users (u_id),
-    CONSTRAINT purchase_course_fk FOREIGN KEY (course_id) REFERENCES Courses (course_id)
+    CONSTRAINT Purchase_pk PRIMARY KEY (user_token, course_cms_id)
 );
--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, request
 from dotenv import load_dotenv
 import os
 from DBConnector import execute_query
 import json
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -10,18 +11,26 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route("/customer/<user_id>/purchases.json")
+@app.route("/customer/<user_id>/purchases.json", methods=['GET', 'POST'])
 def customer_purchases(user_id):
-    query = '''
-            SELECT u.u_id, c.course_name, c.cms_id
-            FROM users AS u LEFT  JOIN purchases AS p ON u.u_id = p.u_id
-            LEFT  JOIN courses AS c ON c.course_id = p.course_id
-            WHERE u.token = %s'''
-    vars = (user_id,)
-    result = execute_query(query, vars)
+    if request.method == 'GET':
+        query = '''
+                SELECT u.u_id, c.course_name, c.cms_id
+                FROM users AS u LEFT  JOIN purchases AS p ON u.u_id = p.u_id
+                LEFT  JOIN courses AS c ON c.course_id = p.course_id
+                WHERE u.token = %s;
+                '''
+        vars = (user_id,)
+        result = execute_query(query, vars)
 
-    return json.dumps(result)
-
+        return json.dumps(result)
+    
+    elif request.method == 'POST':
+        query = '''
+                INSERT INTO purchases (u_id, course_id, price, purchase_date)
+                VALUES (SELECT u_id FROM users WHERE token = %s;,'%s','%s','%s');
+                '''
+        p_date = datetime.now()
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0' )
