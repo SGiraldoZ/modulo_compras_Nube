b0f47ace0dacc1fb363d45640f6de8a97726eff5
Sebas <sebasgizu@gmail.com>
2023-09-10 10:02:37 -0500

    Comment out mysql connection

--- DBConnector.py
+++ DBConnector.py
@@ -3,7 +3,7 @@ import mysql.connector
 import os
 from dotenv import load_dotenv
 
-#load_dotenv()
+load_dotenv()
 username = os.getenv('DB_USERNAME')
 password = os.getenv('DB_PASSWORD')
 host = os.getenv('DB_URL')
@@ -13,16 +13,8 @@ database = os.getenv('DB_NAME')
 
 def connect():
 
-    # Create a connection object.
-    # conn = psycopg2.connect(
-    #     database=database,
-    #     user=username,
-    #     password=password,
-    #     host=host,
-    #     port=port,
-    # )
-
-    conn = mysql.connector.connect(
+    #Create a connection object.
+    conn = psycopg2.connect(
         database=database,
         user=username,
         password=password,
@@ -30,14 +22,22 @@ def connect():
         port=port,
     )
 
+    # conn = mysql.connector.connect(
+    #     database=database,
+    #     user=username,
+    #     password=password,
+    #     host=host,
+    #     port=port,
+    # )
+
     return conn
 
 def execute_query(query, vars=None):
     conn = connect()
-    #cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-    cur = conn.cursor(dictionary=True)
-    # cur.execute(query, vars=vars)
-    cur.execute(query, vars)
+    cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    # cur = conn.cursor(dictionary=True)
+    cur.execute(query, vars=vars)
+    # cur.execute(query, vars)
 
     # Fetch the results.
     rows = cur.fetchall()
@@ -47,9 +47,10 @@ def execute_query(query, vars=None):
 
 def execute_query_commit(query, vars):
     conn = connect()
-    # cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-    cur = conn.cursor(dictionary=True)
-    cur.execute(query, vars)
+    cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    # cur = conn.cursor(dictionary=True)
+    # cur.execute(query, vars)
+    cur.execute(query, vars=vars)
         
     conn.commit()
     conn.close()
